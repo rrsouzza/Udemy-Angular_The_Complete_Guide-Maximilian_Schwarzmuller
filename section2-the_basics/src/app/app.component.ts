d01c7680f1f7942d26c200160bb0cdccdafa0a80
Ricardo Rodrigues <rr_souza@outlook.com>
2022-02-22 10:33:15 -0300

    section2-the_basics / component templates, styles and selectors

--- section2-the_basics/src/app/app.component.ts
+++ section2-the_basics/src/app/app.component.ts
@@ -3,7 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //styleUrls: ['./app.component.css']
+  //É possível ter mais de um arquivo .css referenciando estilo em um componente, ou múltiplas definições in-line. Por isso é necessário os [] para definir o style, pois ele recebe um array de argumentos ou expressões.
+  styles: [`
+    h3{
+      color: dodgerblue;
+    }
+  `]
 })
 export class AppComponent {
   title = 'section2-the_basics';
--- section2-the_basics/src/app/servers/servers.component.ts
+++ section2-the_basics/src/app/servers/servers.component.ts
@@ -1,8 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
-  templateUrl: './servers.component.html',
+
+  selector: 'app-servers',    // escrito dessa forma, seu seletor é uma tag HTML.
+  //selector: '[app-servers]',  -> escrito dessa forma, seu seletor é um atributo. (ex: <div app-servers></div>)
+  //selector: '.app-servers',
+
+  //templateUrl: './servers.component.html',
+
+  //Todo componente *deve* ter um template. Seja referenciando um link de um arquivo externo (como o servers.component.html, ou um html interno, como escrito abaixo:)
+  template: `
+  <app-server></app-server>
+  <app-server></app-server>`,
+  // ` ` permitem escrever uma expressão em múltiplas linhas
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
