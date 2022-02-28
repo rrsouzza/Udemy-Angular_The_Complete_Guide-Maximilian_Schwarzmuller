28bfe544392951a59fc0622895704447381f799a
Ricardo Souza <rr_souza@outlook.com>
2022-02-28 12:25:59 -0300

    section5 updated

--- section5-components_and_dataBinding/src/app/server-element/server-element.component.ts
+++ section5-components_and_dataBinding/src/app/server-element/server-element.component.ts
@@ -1,16 +1,68 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  ElementRef,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
 })
-export class ServerElementComponent implements OnInit {
-  @Input('srvElement') element!: { type: string; name: string, content: string};
+export class ServerElementComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
+  // @Input('srvElement') element!: { type: string; name: string; content: string };
   // @Input aceita um argumento para definir um alias para a propriedade. No caso acima, 'srvElement' passa a ser o alias por qual a propriedade element será chamado, tornando obrigatório o seu uso. Ou seja, 'element' não será mais reconhecido como uma propriedade em escopos externos à essa classe, tornando obrigatório o uso do alias 'srvElement'.
+  @Input() name: string | undefined;
+  @ViewChild('heading', { static: true }) header?: ElementRef;
 
-  constructor() { }
+  constructor() {
+    console.log('Constructor called!');
+  }
+
+  // É possível recuperar informações de um elemento assim que mudanças forem feitas à ele. É possível, por exemplo, recuperar o seu valor anterior à mudança feita.
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges called!');
+    console.log(changes);
+  }
 
   ngOnInit() {
+    console.log('ngOnInit called!');
+    console.log('Text Content:' + this.header?.nativeElement.textContent);
+  }
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck called!');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit called!');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked called!');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit called!');
+    console.log('Text Content:' + this.header?.nativeElement.textContent);
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked called!');
+  }
+
+  // ngOnDestroy permite executar algo no momento em que o componente é descartado.
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy called!');
   }
 }
