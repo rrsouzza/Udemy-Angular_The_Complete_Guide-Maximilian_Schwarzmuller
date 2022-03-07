1773ab50e1a94c6f8e093c476645c6d20116a9e8
Ricardo <rrsouza@outlook.com>
2022-03-07 11:58:03 -0300

    section8 finished

---
+++ section8-courseProject_directives/src/app/app.component.spec.ts
@@ -0,0 +1,31 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'section6-courseProject_components_and_dataBinding'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('section6-courseProject_components_and_dataBinding');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('section6-courseProject_components_and_dataBinding app is running!');
+  });
+});
---
+++ section8-courseProject_directives/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -0,0 +1,25 @@
+import { ViewChild, EventEmitter, Output, ElementRef, Component, OnInit } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
+
+@Component({
+  selector: 'app-shopping-edit',
+  templateUrl: './shopping-edit.component.html',
+  styleUrls: ['./shopping-edit.component.css'],
+})
+export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput', { static: false }) nameInputRef!: ElementRef;
+  @ViewChild('amountInput', { static: false }) amountInputRef!: ElementRef;
+  // ingredientAdded = new EventEmitter<{ name: string; amount: number }>();
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onAddItem() {
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingAmount = this.amountInputRef.nativeElement.value;
+    const newIngredient = new Ingredient(ingName, ingAmount);
+    this.ingredientAdded.emit(newIngredient);
+  }
+}
