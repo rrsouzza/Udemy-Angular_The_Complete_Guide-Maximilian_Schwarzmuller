ada5620a39f9f4e10f70fda96e9461439f025194
rrsouzza <rrsouza@outlook.com>
2022-03-15 10:57:38 -0300

    section9 finished

--- section9_first_assignment/src/app/users.service.ts
+++ section9_first_assignment/src/app/users.service.ts
@@ -11,12 +11,12 @@ export class UserService {
   setToActive(id: number) {
     this.activeUsers.push(this.inactiveUsers[id]);
     this.inactiveUsers.splice(id, 1);
-    this.counterService.incrementInactiveToActive;
+    this.counterService.incrementInactiveToActive();
   }
 
   setToInactive(id: number) {
     this.inactiveUsers.push(this.activeUsers[id]);
     this.activeUsers.splice(id, 1);
-    this.counterService.incrementActiveToInactive;
+    this.counterService.incrementActiveToInactive();
   }
 }
