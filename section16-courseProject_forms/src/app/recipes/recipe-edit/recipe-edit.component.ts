ffde31d8cdf74ecf5198c715bc756a55405dd207
Ricardo Rodrigues <66682713+rrsouzza@users.noreply.github.com>
2022-08-04 15:39:11 -0300

    section16-finished

--- section16-courseProject_forms/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ section16-courseProject_forms/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormControl, FormGroup } from '@angular/forms';
-import { ActivatedRoute, Params } from '@angular/router';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { RecipeService } from '../recipe.service';
 
 @Component({
@@ -13,7 +13,7 @@ export class RecipeEditComponent implements OnInit {
   editMode = false;
   recipeForm!: FormGroup;
 
-  constructor(private route: ActivatedRoute, private recipeService: RecipeService) {}
+  constructor(private route: ActivatedRoute, private recipeService: RecipeService, private router: Router) {}
 
   ngOnInit(): void {
     this.route.params.subscribe((params: Params) => {
@@ -38,8 +38,8 @@ export class RecipeEditComponent implements OnInit {
         for (const ingredient of recipe.ingredients) {
           recipeIngredients.push(
             new FormGroup({
-              name: new FormControl(ingredient.name),
-              amount: new FormControl(ingredient.amount),
+              name: new FormControl(ingredient.name, Validators.required),
+              amount: new FormControl(ingredient.amount, [Validators.required, Validators.pattern(/^[1-9]+[0-9]*$/)]),
             })
           );
         }
@@ -47,14 +47,50 @@ export class RecipeEditComponent implements OnInit {
     }
 
     this.recipeForm = new FormGroup({
-      name: new FormControl(recipeName),
-      imagePath: new FormControl(recipeImagePath),
-      description: new FormControl(recipeDescription),
+      name: new FormControl(recipeName, Validators.required),
+      imagePath: new FormControl(recipeImagePath, Validators.required),
+      description: new FormControl(recipeDescription, Validators.required),
       ingredients: recipeIngredients,
     });
   }
 
+  onAddIngredient() {
+    (<FormArray>this.recipeForm.get('ingredients')).push(
+      new FormGroup({
+        name: new FormControl(null, Validators.required),
+        amount: new FormControl(null, [Validators.required, Validators.pattern(/^[1-9]+[0-9]*$/)]),
+      })
+    );
+  }
+
+  onDeleteIngredient(index: number) {
+    (<FormArray>this.recipeForm.get('ingredients')).removeAt(index);
+  }
+  onDeleteAllIngredients() {
+    (<FormArray>this.recipeForm.get('ingredients')).clear();
+  }
+
+  get controls() {
+    return (<FormArray>this.recipeForm.get('ingredients')).controls;
+  }
+
   onSubmit() {
-    console.log(this.recipeForm);
+    // const newRecipe = new Recipe(
+    //   this.recipeForm.value['name'],
+    //   this.recipeForm.value['description'],
+    //   this.recipeForm.value['imagePath'],
+    //   this.recipeForm.value['ingredients']
+    // );
+    if (this.editMode) {
+      this.recipeService.updateRecipe(this.id, this.recipeForm.value);
+    } else {
+      this.recipeService.addRecipe(this.recipeForm.value);
+    }
+
+    this.onCancel();
+  }
+
+  onCancel() {
+    this.router.navigate(['../'], { relativeTo: this.route });
   }
 }
--- section16-courseProject_forms/src/app/recipes/recipe.service.ts
+++ section16-courseProject_forms/src/app/recipes/recipe.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { NumberValueAccessor } from '@angular/forms';
+import { Subject } from 'rxjs';
 
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
@@ -8,28 +10,20 @@ import { Recipe } from './recipe.model';
 @Injectable()
 export class RecipeService {
   // prettier-ignore
+  recipesChanged = new Subject<Recipe[]>();
+
   private recipes: Recipe[] = [
-    new Recipe(
-      'Tasty Schnitzel',
-      'A super-tasty schnitzel - just awesome!',
-      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
-      [
-        new Ingredient('Meat', 1),
-        new Ingredient('French Fries', 20),
-      ]
-    ),
-    new Recipe(
-      'Big fat burger',
-      'What else you need to say?',
-      'https://www.stockvault.net/data/2016/04/19/194208/preview16.jpg',
-      [
-        new Ingredient('Bread', 2),
-        new Ingredient('Meat', 1),
-        new Ingredient('Cheese Slice', 2),
-        new Ingredient('Tomatoe Slice', 1),
-        new Ingredient('Lettuce', 1)
-      ]
-    ),
+    new Recipe('Tasty Schnitzel', 'A super-tasty schnitzel - just awesome!', 'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG', [
+      new Ingredient('Meat', 1),
+      new Ingredient('French Fries', 20),
+    ]),
+    new Recipe('Big fat burger', 'What else you need to say?', 'https://www.stockvault.net/data/2016/04/19/194208/preview16.jpg', [
+      new Ingredient('Bread', 2),
+      new Ingredient('Meat', 1),
+      new Ingredient('Cheese Slice', 2),
+      new Ingredient('Tomatoe Slice', 1),
+      new Ingredient('Lettuce', 1),
+    ]),
   ];
 
   constructor(private slService: ShoppingListService) {}
@@ -45,5 +39,20 @@ export class RecipeService {
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
     this.slService.addIngredients(ingredients);
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  deleteRecipe(index: number) {
+    this.recipes.splice(index, 1);
+    this.recipesChanged.next(this.recipes.slice());
   }
 }
