0046f2f3e668e39dacc4b8a5fbddd1674a2a6146
Ricardo Rodrigues <66682713+rrsouzza@users.noreply.github.com>
2022-08-19 16:24:19 -0300

    section20 started

---
+++ section20-authentication_and_route_protection_in_angular/src/app/shared/data-storage.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { exhaustMap, map, take, tap } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
+import { Recipe } from '../recipes/recipe.model';
+import { RecipeService } from '../recipes/recipe.service';
+
+@Injectable()
+export class DataStorageService {
+  constructor(private http: HttpClient, private recipeService: RecipeService, private authService: AuthService) {}
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    this.http.put('https://udemymaxschwartz-angular-default-rtdb.firebaseio.com/recipes.json', recipes).subscribe((response) => {
+      console.log(response);
+    });
+  }
+
+  fetchRecipes() {
+    //take() allows to unsubscribe from the observable immediately after getting the requested value/property.
+    return this.authService.user.pipe(
+      take(1),
+      exhaustMap((user) => {
+        return this.http.get<Recipe[]>('https://udemymaxschwartz-angular-default-rtdb.firebaseio.com/recipes.json', {
+          params: new HttpParams().set('auth', user.token),
+        });
+      }),
+      map((recipes) => {
+        return recipes.map((recipe) => {
+          return { ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] };
+        });
+      }),
+      tap((recipes) => {
+        this.recipeService.setRecipes(recipes);
+      })
+    );
+  }
+}
